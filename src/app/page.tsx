18d0acca82ae7b95aaf456f18a0f0ef2ef29bcb6
sobiashaukat <126345014+sobiashaukat@users.noreply.github.com>
2024-04-16 01:32:17 +0500

    links updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { Modal } from "@/components/modal";
 import { AlertDialogDemo } from "@/components/sign-up";
 import { Form } from "@/components/form";
@@ -6,10 +5,15 @@ import { DialogDemo } from "@/components/store";
 import { CardWithForm } from "@/components/create-store";
 import { Business } from "@/components/business.info";
 import { Address } from "@/components/address";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col gap-8 p-24">
+        <Link href="/billing">Billing</Link>
+        <Link href="/account">Account</Link>
+      </div>
       <div className="flex flex-col gap-4">
         <Modal></Modal>
         <AlertDialogDemo></AlertDialogDemo>
